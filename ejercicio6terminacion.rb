57787e44d6cd42a8462f705b475c731592b6b5ff
JonathanCastr <jcastro@itpro.cl>
2022-07-06 11:44:47 -0400

    listolisto

--- ejercicio6terminacion.rb
+++ ejercicio6terminacion.rb
@@ -4,3 +4,15 @@ for a in 0..19 do
     texto.rotate! 1
     puts
 end
+texto1 =  ["+","+","*","~","~","*","+","+","*"]
+for a in 0..19 do
+    print *texto1
+    texto1.rotate! 1
+    puts
+end
+texto2 =  ["=","|","/","|","/","|","=","|","/","|"]
+for a in 0..19 do
+    print *texto2
+    texto2.rotate! 1
+    puts
+end
