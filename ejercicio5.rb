b3fa6031fbc065ef21c1a6921c6a50ab3e03f652
JonathanCastr <jcastro@itpro.cl>
2022-07-18 15:10:23 -0400

    .

---
+++ ejercicio5.rb
@@ -0,0 +1,25 @@
+numero = {1 =>"I",
+2=>"II",
+3=>"IV",
+4=>"V"
+5=>"VI",
+7=>"VII"
+8=>"VII",
+9=>"IX",
+10=>"X"
+"I"=>1,
+"II"=>2,
+"II"=>3,
+"IV"=>4,
+"V"=>5,
+"VI"=>6,
+"VII"=>7,
+"VIII"=>8,
+"IX"=>9,
+"X"=>10}
+puts "Ingrese un numero del 1 al 10 para pasarlo a romano o del I al X para pasarlo a decimal"
+valor= gets.chomp
+
+
+    
+end
