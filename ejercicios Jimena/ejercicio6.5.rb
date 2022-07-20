c8f6be992fb26af6a1f27a99489a1a56c4a32013
JimenaGitHub <jaliagasanhueza@gmail.com>
2022-07-20 10:59:23 -0400

    ok

--- ejercicios Jimena/ejercicio6.5.rb
+++ ejercicios Jimena/ejercicio6.5.rb
@@ -1,16 +1,19 @@
 #Convierte número decimal a romano
 
-puts "Escribre un número"
+puts "Escribre un número:"
 
 numeros = {
-    I:1,
-    II:2,
-    III:3,
-    IV:4,
-    V:5,
-    VI:6,
-    VII:7,
-    VIII:8,
-    IX:9,
-    X:10
+    'I' => 1,
+    'II'=> 2,
+    'III'=> 3,
+    'IV' => 4,
+    'V'=> 5,
+    'VI'=> 6,
+    'VII'=> 7,
+    'VIII' => 8,
+    'IX'=> 9,
+    'X'=> 10
 }
+numeros[1]
+=> 'I'
+end
