f6ab6b7c80e022beba8133cd8f4ad978acb1ccf0
JonathanCastr <jcastro@itpro.cl>
2022-07-19 22:01:18 -0400

    ok

--- ejercicio5.rb
+++ ejercicio5.rb
@@ -16,13 +16,14 @@ numero =
         print "escribe un numero del 1 al 10 en decimal o romano"
         puts
         respuesta = gets.chomp
-        if respuesta.to_i != respuesta.to_s && numero.has_key?(respuesta.upcase)
-            puts "el numero romano en decimal seria #{numero [respuesta.upcase]}"
+        if respuesta.to_i != respuesta.to_s && numero.has_key?(respuesta.downcase) #como en tu caso es en minusculas .downcase es el modulo
+            puts "el numero romano en decimal seria #{numero [respuesta.downcase]}" # mismo aqui
         elsif respuesta.to_i > 0 && respuesta.to_i <=10
             decimal = numero.invert
             puts "el numero decimal en numero romano seria #{decimal [respuesta.to_i]}"
         else
-            puts numero.default = "no encontrado"
+ #           puts numero.default = "no encontrado" #no es necesario poner el numero.default ahora si lo quieres poner debe ser asi
+            puts "#{numero.default}: No encontrado"
         end
             
     
