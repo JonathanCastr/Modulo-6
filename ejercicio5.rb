d9d2ce15f9df2731d87b3b85fdd7608fad3e6564
JonathanCastr <jcastro@itpro.cl>
2022-07-19 20:23:26 -0400

    ok

--- ejercicio5.rb
+++ ejercicio5.rb
@@ -1,11 +1,12 @@
 numero = {"I"=>1,"II"=>2,"III"=>3,"IV"=>4,"V"=>5,"VI"=>6,"VII"=>7,"VIII"=>8,"IX"=>9,"X"=>10}
+
 puts "Ingrese un numero del 1 al 10 para pasarlo a romano o del I al X para pasarlo a decimal"
-valor= gets.chomp
+valor  = gets.chomp
 if valor.to_i != valor.to_s && numero.has_key?(valor.upcase)
-    puts "Su numero romano en decimal es #{numero[valor.upcase]}"    
-elsif valor.to_i > 0 && valor.to_i <=10
-    integer = numero.invert
-    puts "Su numero decimal en romano es #{integer[valor.to_i]}"   
+    puts "Su numero romano en decimal es #{numero[valor.upcase]}"
+elsif valor.to_i > 0 && valor.to_i <= 5
+    nueva_variable = numero.invert
+    puts "Su numero decimal en romano es #{nueva_variable[valor.to_i]}"
 else 
-    puts "Fuera del rango indicado" 
+    puts "El dato ingresado no es válido"
 end
