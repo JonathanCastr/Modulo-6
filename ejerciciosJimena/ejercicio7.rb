2a3e1985f8f12fdc8fbf1366a8d1cdbaf956cbca
JimenaGitHub <jaliagasanhueza@gmail.com>
2022-07-21 19:00:25 -0400

    no me resulta

--- ejerciciosJimena/ejercicio7.rb
+++ ejerciciosJimena/ejercicio7.rb
@@ -1,7 +1,5 @@
 require_relative 'radio.rb'
-r1 = Radio.new(radio.volumen(“+”), radio.volumen(“-”) )
-r1.radio.estatus()
-radio2 = Radio.ner(radio.frecuencia(“AM”), radiofrecuencia(“FM”))
-r2.radio.estatus()
+radio = Radio.new
 puts radio.estatus
-
+radio.radio_volumen = "27"
+radio.radio_frecuencia = "FM”
--- ejerciciosJimena/radio.rb
+++ ejerciciosJimena/radio.rb
@@ -1,17 +1,13 @@
 class Radio
     
-    def initialize(volumen, frecuencia)
-        @volumen = volumen
-        @frecuencia = frecuencia
+    def radio.volumen()
+        "#{radio_volumen[“+”]} #{radio_volumen[“-”]}"
+        gets volumen [1..30]
     end
 
-    def subirvolumen()
-        puts @volumen + (1..30)
+    def radio.frecuencia()
+        "#{radio_frecuencia[“AM”]} #{radio_frecuencia[“FM”]}"
+        gets_frecuencia[]
+        
     end
-
-    def cambiarfrecuencia()
-        puts @frecuencia + (AM & FM)
-    end
-
-    
 end
